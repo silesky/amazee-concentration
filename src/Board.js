147aeb242349448326fe8a3ca308198ed4d06a3c
silesky <seth.silesky@gmail.com>
2017-02-27 13:21:21 -0600

    rename App to Board

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './board.css';
+
+class Board extends Component {
+  render() {
+    return (
+      <h1>Concentration</h1>
+    );
+  }
+}
+
+export default Board;
